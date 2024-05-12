a526314ea99c4da8a22b0e491c7b647e0ea5e428
hoanganh0831 <hoanganh0831@gmail.com>
2024-05-12 11:09:33 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Git!'));
+app.get('/', (req, res) => res.send('Hi Git for 2times!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
